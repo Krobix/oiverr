6d5b8e62dbe8a923cb6a908f7ef15c079be13038
Krobix <mrzlime@gmail.com>
2020-10-17 13:41:55 +0000

    add base files n stuff

---
+++ src/oliverr/object.go
@@ -0,0 +1,10 @@
+package oliverr
+
+/* This is the types "enum" */
+const (
+	IntT = iota
+	StrT
+	FloatT
+	FptrT //function pointer
+	DictT //dictionary type; basically the same as what objects are in otehr languages
+)
