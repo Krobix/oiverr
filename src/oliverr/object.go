bd29434574e1edf48249b8e11f8d62ffca442c6e
Krobix <mrzlime@gmail.com>
2020-10-19 14:36:36 -0400

    start adding object structure

--- src/oliverr/object.go
+++ src/oliverr/object.go
@@ -6,5 +6,30 @@ const (
 	StrT
 	FloatT
 	FptrT //function pointer
-	DictT //dictionary type; basically the same as what objects are in otehr languages
+	DictT //dictionary type; basically a map
 )
+
+/*Object represents all objects in general */
+type Object interface {
+	/*Conversion methods*/
+	getStr() string
+	getInt() int64
+	getFloat() float64
+	getFptr() int64
+	getDict() map[Object]Object //Will not work for all types, probably only for dict type itself
+}
+
+/* Individual type structs */
+
+type StringObject struct {
+	value string
+}
+
+type IntObject struct {
+	value int64
+}
+
+type FloatObject struct {
+	value float64
+}
+
